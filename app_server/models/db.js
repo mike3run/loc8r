ff69cc7ad766647ddcf061d32cb444cc1a0fbb96
mike3run <mpalauzarza@gmail.com>
2016-06-03 15:25:06 -0500

    added model and schema

---
+++ app_server/models/db.js
@@ -0,0 +1,50 @@
+var mongoose = require('mongoose');
+
+var dbURI = 'mongodb://localhost/Loc8r';
+
+mongoose.connect(dbURI);
+
+
+mongoose.connection.on('connected', function() {
+  console.log('Mongoose connected to ' + dbURI);
+});
+
+mongoose.connection.on('error', function(err) {
+  console.log('Mongoose connection error: ' + err );
+});
+
+mongoose.connection.on('disconnected', function() {
+  console.log('Mongoose has been disconnected');
+});
+
+// Gracefull shutdowns
+var gracefulShutDown = function(msg, callback) {
+  mongoose.connection.close(function() {
+    console.log('Mongoose disconnected through ' + msg);
+    callback();
+  });
+};
+
+// When NodeMond restarts
+process.once('SIGUSR2', function(){
+  gracefulShutDown('nodemon restart', function() {
+    process.kill(process.pid, 'SIGUSR2');
+  });
+});
+
+// For app termination
+process.on('SIGINT', function(){
+  gracefulShutDown('app termination', function() {
+    process.exit(0);
+  });
+});
+
+// For heroku app termination
+process.on('SIGTERM', function() {
+  gracefulShutDown('Heroku app shutdown', function() {
+    process.exit(0);
+  });
+});
+
+// Lets require everything else
+require('./locations');
